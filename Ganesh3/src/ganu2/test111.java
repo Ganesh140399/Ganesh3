92f028a81d1c18501e7c7679801f66c9e4ad6af3
Lenovo <Lenovo@LAPTOP-91EJ1UJ3>
2022-10-27 13:26:20 +0530

    Commit2

---
+++ Ganesh3/src/ganu2/test111.java
@@ -0,0 +1,14 @@
+package ganu2;
+
+public class test111 
+{
+	public static void main(String[] args)
+	  {
+		
+		  System.out.println("My name is Ganesh Patil");
+		  System.out.println("I have performed Automation Testing");
+		   
+	  }
+	
+	
+}
